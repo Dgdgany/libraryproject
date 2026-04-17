dc098515787bc9a84f0bc81b78b8aa048d242f91
Pixel <431109390@qu.edu.sa>
2026-04-17 17:13:21 +0300

    Lab 6

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
 
+def __getBooksList():
+    book1 = {'id': 12344321, 'title': 'Harry Potter', 'author': 'J. K. Rowling'}
+    book2 = {'id': 56788765, 'title': 'DUNE', 'author': 'Frank Herbert'}
+    book3 = {'id': 43211234, 'title': 'Alice in Wonderland', 'author': 'Lewis Carroll'}
+    return [book1, book2, book3]
+
+def search(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+        
+       
+        books = __getBooksList()
+        newBooks = []
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower(): 
+                contained = True
+            if not contained and isAuthor and string in item['author'].lower(): 
+                contained = True
+            
+            if contained: 
+                newBooks.append(item)
+        
+        return render(request, 'bookmodule/search.html', {'books': newBooks})
+    
+    return render(request, 'bookmodule/search.html')
+
+
 def index(request):
     return render(request, "bookmodule/index.html")
 
@@ -11,6 +41,7 @@ def viewbook(request, bookId):
 
 def aboutus(request):
     return render(request, "bookmodule/aboutus.html")
+
 
 def formatting(request):
     return render(request, 'bookmodule/formatting.html')
